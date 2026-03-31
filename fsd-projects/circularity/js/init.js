81df5c841b5cb68ff7e9440602d39c9090b366d3
LucasTaylor7099957 <llt7099957@lpssonline.com>
2026-03-31 14:22:06 +0000

    todo 8 complete

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -59,15 +59,14 @@ for (var loopsCompleted = 0; loopsCompleted < 175; loopsCompleted++) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-physikz.updatePosition(circles[ 0 ])
+for (var i = 0; i < circles.length; i++) {
 
-physikz.updatePosition(circles[ 1 ])
+               physikz.updatePosition(circles[i]);
 
-physikz.updatePosition(circles[ 2 ])
+               game.checkCirclePosition(circles[i]);
 
-physikz.updatePosition(circles[ 3 ])
 
-physikz.updatePosition(circles[ 4 ])
+}
                       
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
 game.checkCirclePosition(circles[ 0 ])
